4931b1c9b314237f5b22163bcdc68b80ce760d45
Sameow <sameow.ong@gmail.com>
2019-07-07 15:08:57 +0800

    Final

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './app.js',
+    entry: './src/app.js',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
